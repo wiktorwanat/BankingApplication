971faa801d2b0f8d7281c251556237381acf8d72
wiktorwanat <46346155+wiktorwanat@users.noreply.github.com>
2020-03-01 20:38:43 +0100

    Add files via upload

---
+++ BankingApplicationDataBasea.sql
@@ -0,0 +1,7 @@
+UserID,FirstName,LastName,AccountNumber,AccountBalance,Login,Password,AddressEmail
+7,Kamil,Kaluza,25162986,250,kamilkaluza,123123,kaluzakamil@gmail.com
+8,Andrzej,Nowak,58687540,12333.5,andrzejnowak,qwerty,andrzej@gmail.com
+9,Andrzej,Noga,47997270,250.56,anoga,noga123,anoga@gmail.com
+12,Michal,Kowal,64546371,123.33,michalk,kowal123,michalk@gmail.com
+13,Wiktor,Nowak,89841836,123233.33,wiktor123,wikus,akanguly@gmail.com
+14,Adam,Byk,22531470,150,adambyk,adam123123,adambyk@gmail.com
